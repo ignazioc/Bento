87bd766ccdd303d20c20b3194ab85b734a1956bd
IgnazioC <ignazioc@gmail.com>
2021-08-01 17:16:47 +0200

    WIP

--- config.js
+++ config.js
@@ -37,40 +37,16 @@ const CONFIG = {
   cards: [
     {
       id: '1',
-      name: 'Github',
-      icon: 'github',
-      link: 'https://github.com/',
+      name: 'Gmail',
+      icon: 'mail',
+      link: 'https://mail.google.com/',
     },
     {
       id: '2',
-      name: 'Mail',
-      icon: 'mail',
-      link: 'https://mail.protonmail.com/',
-    },
-    {
-      id: '3',
-      name: 'Todoist',
-      icon: 'trello',
-      link: 'https://calendar.google.com/calendar/r',
-    },
-    {
-      id: '4',
-      name: 'Calendar',
-      icon: 'calendar',
-      link: 'https://calendar.google.com/calendar/r',
-    },
-    {
-      id: '5',
       name: 'Reddit',
-      icon: 'bookmark',
-      link: 'https://reddit.com',
-    },
-    {
-      id: '6',
-      name: 'Odysee',
-      icon: 'youtube',
-      link: 'https://odysee.com/',
-    },
+      icon: 'external-link',
+      link: 'https://www.reddit.com/',
+    }
   ],
 
   // ┬  ┬┌─┐┌┬┐┌─┐
